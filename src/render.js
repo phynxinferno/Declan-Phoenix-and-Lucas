93098212323c41783b846b08eb6d8f4b5f3542d7
phynxinferno <phynxinferno1@gmail.com>
2022-03-30 12:19:04 -0400

    applied fonts, background

---
+++ src/render.js
@@ -0,0 +1,15 @@
+/*document.addEventListener('DOMContentLoaded', function() {
+    console.log('test')
+    setInterval(function() {
+        var rand = Math.floor(Math.random() * 10)
+        switch(rand) {
+            case rand < 3:
+                alert('Adobe flash player is out of date. Would you like to download the latest version for free?')
+            case rand > 3 && rand < 6:
+                alert('Congratualations on being our 1 000 000th visitor. Click to claim your free Samsung zFold 3')
+            case rand > 6:
+                alert("We've been trying to reach you concerning your car's extended warranty. Please contact tech support at 296 321 214 before your warranty is terminated")
+        }
+        alert("We've been trying to reach you concerning your car's extended warranty. Please contact tech support at 296 321 214 before your warranty is terminated")
+    }, 1000) 
+})*/
